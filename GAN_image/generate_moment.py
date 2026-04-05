71c0b766efe15e8370e10c99ba9c33ee69558d1b
KNRai <niturai20129617@iisertvm.ac.in>
2026-04-05 16:22:10 +0530

    moment

--- GAN_image/generate_moment.py
+++ GAN_image/generate_moment.py
@@ -8,67 +8,110 @@ import re
 md = mom()
 
 # The Directory of all images  
-gr_img = "testing_image/testing_image/"
-data_path = os.path.join(gr_img, "image_*_target.npy")
-files1 = glob.glob(data_path)
-files1.sort(key=lambda x: [int(c) if c.isdigit() else c for c in re.split(r'(\d+)', x)])  # Natural sort by numeric part
+gr_img6 = "result/six/testing_image/"
+gr_img9 = "result/nine/testing_image/testing_image/"
+data_path6 = os.path.join(gr_img6, "image_*_target.npy")
+data_path9 = os.path.join(gr_img9, "image_*_target.npy")
+files16 = glob.glob(data_path6)
+files16.sort(key=lambda x: [int(c) if c.isdigit() else c for c in re.split(r'(\d+)', x)])  # Natural sort by numeric part
+files19 = glob.glob(data_path9)
+files19.sort(key=lambda x: [int(c) if c.isdigit() else c for c in re.split(r'(\d+)', x)])  # Natural sort by numeric part
 
-pr_img = "testing_image/testing_image/"
-data_path = os.path.join(pr_img, "image_*_prediction.npy")
-files2 = glob.glob(data_path)
-files2.sort(key=lambda x: [int(c) if c.isdigit() else c for c in re.split(r'(\d+)', x)])  # Natural sort by numeric part
+data_path6 = os.path.join(gr_img6, "image_*_prediction.npy")
+data_path9 = os.path.join(gr_img9, "image_*_prediction.npy")
+files26 = glob.glob(data_path6)
+files26.sort(key=lambda x: [int(c) if c.isdigit() else c for c in re.split(r'(\d+)', x)])  # Natural sort by numeric part
+files29 = glob.glob(data_path9)
+files29.sort(key=lambda x: [int(c) if c.isdigit() else c for c in re.split(r'(\d+)', x)])  # Natural sort by numeric part
 
-ground_img = []
-pred_img = []
+ground_img6 = []
+pred_img6 = []
+ground_img9 = []
+pred_img9 = []
 # Loop over both lists together
-for f1, f2 in zip(files1, files2):
-    data1 = np.load(f1)   # load ground truth
-    data2 = np.load(f2)   # load prediction
-    ground_img.append(data1)
-    pred_img.append(data2)
+for f16, f26, f19, f29 in zip(files16, files26, files19, files29):
+    data16 = np.load(f16)   # load ground truth
+    data26 = np.load(f26)   # load prediction
+    
+    data19 = np.load(f19)   # load ground truth
+    data29 = np.load(f29)   # load prediction
+    
+    ground_img6.append(data16.squeeze())
+    pred_img6.append(data26.squeeze())
+    
+    ground_img9.append(data19.squeeze())
+    pred_img9.append(data29.squeeze())
 
 # Convert lists to arrays
-gr_img = np.array(ground_img)
-pr_img = np.array(pred_img)
-print(gr_img.shape)
+gr_img6 = np.array(ground_img6)
+pr_img6 = np.array(pred_img6)
 
-plt.rcParams.update({'font.size': 12})
-plt.rcParams["figure.figsize"] = [6,6]
-plt.rcParams['axes.facecolor']='ivory'
-plt.rcParams["font.weight"] = "bold"
-plt.rcParams["axes.labelweight"] = "bold"
+gr_img9 = np.array(ground_img9)
+pr_img9 = np.array(pred_img9)
+
+print(gr_img6.shape)
+
+# Calculate the Scatter of centroid and moments for ground truth and predicted image
+SixSc, SixS11, SixS20, SixS02, SixS12, SixS21, SixS30, SixS03 = md.mom_cal(gr_img6, pr_img6)
+nineSc, nineS11, nineS20, nineS02, nineS12, nineS21, nineS30, nineS03 = md.mom_cal(gr_img9, pr_img9)
+
+print('6Sc, 6S11, 6S20, 6S02, 6S12, 6S21, 6S30, 6S03', SixSc, SixS11, SixS20, SixS02, SixS12, SixS21, SixS30, SixS03)
+print('9Sc, 9S11, 9S20, 9S02, 9S12, 9S21, 9S30, 9S03', nineSc, nineS11, nineS20, nineS02, nineS12, nineS21, nineS30, nineS03)
 
 # visualise the moments of each generated images
-list_g = []
-list_p = []
-for i in range(len(gr_img)):
-    im_g = gr_img[i].squeeze()   # (128,128)
-    im_p = pr_img[i].squeeze()   # (128,128)
-    list_g.append(md.mom_def(im_g))
-    list_p.append(md.mom_def(im_p))
+list_g6 = []
+list_p6 = []
+list_g9 = []
+list_p9 = []
+for i in range(len(gr_img6)):
+    gr6 = gr_img6[i]
+    pr6 = pr_img6[i]
+    gr9 = gr_img9[i]
+    pr9 = pr_img9[i]
+    
+    im_g6 = (gr6*0.5 + 0.5)             # (128,128) (the scaling factor is for 0 to 1 from -1 to 1)
+    im_p6 = (pr6*0.5 + 0.5)             # (128,128)
+    
+    im_g9 = (gr9*0.5 + 0.5)             # (128,128) (the scaling factor is for 0 to 1 from -1 to 1)
+    im_p9 = (pr9*0.5 + 0.5)             # (128,128)
+       
+    list_g6.append(md.mom_def(im_g6))
+    list_p6.append(md.mom_def(im_p6))
+    
+    list_g9.append(md.mom_def(im_g9))
+    list_p9.append(md.mom_def(im_p9))
 
-
-os.makedirs("moments", exist_ok=True)
-
-Mth_g = []
-Mth_p = []
+Mth_g6 = []
+Mth_p6 = []
+Mth_g9 = []
+Mth_p9 = []
 m_name = ['Monopole', 'X-Centroid', 'Y-Centroid', '2nd Moment $\mu_{11}$', 
           '2nd Moment $\mu_{20}$', '2nd Moment $\mu_{02}$', '3rd Moment $\mu_{30}$', 
           '3rd Moment $\mu_{03}$','3rd Moment $\mu_{21}$', '3rd Moment $\mu_{12}$']
 pic = 0
-for i in range(len(list_g[0])):
-    Mth_g = [M[i] for M in list_g]
-    Mth_p = [M[i] for M in list_p]
-    for j in range(len(Mth_g)):
-        plt.plot(Mth_g[j], Mth_p[j], 'o', markeredgecolor='blue', markersize=8, markeredgewidth=1.5)
+for i in range(len(list_g6[0])):
+    plt.rcParams.update({'font.size': 12})
+    plt.rcParams["figure.figsize"] = [6,6]
+    plt.rcParams['axes.facecolor']='ivory'
+    plt.rcParams["font.weight"] = "bold"
+    plt.rcParams["axes.labelweight"] = "bold"
+    Mth_g6 = np.array([M[i] for M in list_g6])
+    Mth_p6 = np.array([M[i] for M in list_p6])
+    Mth_g9 = np.array([M[i] for M in list_g9])
+    Mth_p9 = np.array([M[i] for M in list_p9])
 
+    plt.plot(Mth_g6, Mth_p6, '.', color='green', markersize=9, label='Six Telescope')
+    plt.plot(Mth_g9, Mth_p9, '.', color='red', markersize=9, label='Nine Telescope')
+
+    plt.legend(loc='upper left')
     plt.xlabel('Ground Truth')
     plt.ylabel('Predicted Image')
     plt.ticklabel_format(style='sci', axis='both', scilimits=(0,0))
     plt.title(f'{m_name[i]} for Stellar object', fontweight='bold')
-    #plt.gca().set_aspect('equal', adjustable='box')
     plt.tight_layout()
-    plt.savefig('moments/mom' + str(pic) + '.png')
-    #plt.show()
+    plt.savefig('result/moments/mom' + str(pic) + '.png')
     plt.clf()
-    pic += 1 
+    pic += 1
+    plt.close()
+    
+
--- GAN_image/moment.py
+++ GAN_image/moment.py
@@ -19,9 +19,10 @@ class mom():
           """
     
           height, width = img.shape
-          x = np.arange(0, width)
-          y = np.arange(0, height)
+          x = np.linspace(-64, 64, width)
+          y = np.linspace(-64, 64, height)
           x, y = np.meshgrid(x, y)
+
     
           # monopole
           M00 = np.sum(img)
@@ -42,4 +43,51 @@ class mom():
 
           return M00, mx, my, mu11, mu20, mu02, mu30, mu03, mu21, mu12
           
+      def mom_cal(self, imgG, imgP):
+    
+          n = imgG.shape[0]
+    
+          mxG, myG = [], []
+          mxP, myP = [], []
+    
+          mu11G, mu20G, mu02G, mu30G, mu03G, mu21G, mu12G = [], [], [], [], [], [], []
+          mu11P, mu20P, mu02P, mu30P, mu03P, mu21P, mu12P = [], [], [], [], [], [], []
+
+          for i in range(n):
+              M00g, mxg, myg, mu11g, mu20g, mu02g, mu30g, mu03g, mu21g, mu12g = self.mom_def(imgG[i])
+              M00p, mxp, myp, mu11p, mu20p, mu02p, mu30p, mu03p, mu21p, mu12p = self.mom_def(imgP[i])
+
+              mxG.append(mxg); myG.append(myg)
+              mxP.append(mxp); myP.append(myp)
+
+              mu11G.append(mu11g); mu20G.append(mu20g); mu02G.append(mu02g)
+              mu30G.append(mu30g); mu03G.append(mu03g); mu21G.append(mu21g); mu12G.append(mu12g)
+
+              mu11P.append(mu11p); mu20P.append(mu20p); mu02P.append(mu02p)
+              mu30P.append(mu30p); mu03P.append(mu03p); mu21P.append(mu21p); mu12P.append(mu12p)
+              
+          mxG, myG = np.array(mxG), np.array(myG)
+          mxP, myP = np.array(mxP), np.array(myP)
+
+          mu11G, mu20G, mu02G = np.array(mu11G), np.array(mu20G), np.array(mu02G)
+          mu11P, mu20P, mu02P = np.array(mu11P), np.array(mu20P), np.array(mu02P)
+
+          mu30G, mu03G = np.array(mu30G), np.array(mu03G)
+          mu30P, mu03P = np.array(mu30P), np.array(mu03P)
+
+          mu21G, mu12G = np.array(mu21G), np.array(mu12G)
+          mu21P, mu12P = np.array(mu21P), np.array(mu12P)
           
+          # calculate the scatter in centroid
+          Sc = n**(-1) * np.sqrt(np.sum((mxG - mxP)**2 + (myG - myP)**2))
+          
+          # calculate the scatter in central moment
+          S11 = n**(-1) * np.sqrt(np.sum((mu11G - mu11P)**2))
+          S20 = n**(-1) * np.sqrt(np.sum((mu20G - mu20P)**2))
+          S02 = n**(-1) * np.sqrt(np.sum((mu02G - mu02P)**2))
+          S12 = n**(-1) * np.sqrt(np.sum((mu12G - mu12P)**2))
+          S21 = n**(-1) * np.sqrt(np.sum((mu21G - mu21P)**2))
+          S30 = n**(-1) * np.sqrt(np.sum((mu30G - mu30P)**2))
+          S03 = n**(-1) * np.sqrt(np.sum((mu03G - mu03P)**2))
+          
+          return Sc, S11, S20, S02, S12, S21, S30, S03
